c50a109112d2198e57e2c879e80593e6d801f2cd
Lucas Martins <a79294@ualg.pt>
2023-11-17 15:30:28 +0000

    Meti as opcoes do menu principal em comentarios

--- base/src/main.py
+++ base/src/main.py
@@ -1,9 +1,9 @@
-from clientes import cria_novo_cliente, imprime_lista_de_clientes
-from faturas import cria_nova_fatura, imprime_lista_de_faturas
-from io_ficheiros import (carrega_as_listas_dos_ficheiros,
-                          guarda_as_listas_em_ficheiros)
-from io_terminal import pause
-from veiculos import cria_novo_veiculo, imprime_lista_de_veiculos
+#from clientes import cria_novo_cliente, imprime_lista_de_clientes
+#from faturas import cria_nova_fatura, imprime_lista_de_faturas
+#from io_ficheiros import (carrega_as_listas_dos_ficheiros,
+#                          guarda_as_listas_em_ficheiros)
+#from io_terminal import pause
+#from veiculos import cria_novo_veiculo, imprime_lista_de_veiculos
 
 
 
@@ -45,46 +45,46 @@ def menu():
         ____________________________________________________________________
         """)
 
-        op = input("opcao?").lower()
+        op = input("Selecione: ").lower()
 
         if op == "x":
             exit()
 
-        elif op == "g":
-            guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de_faturas)
+        #elif op == "g":
+            #guarda_as_listas_em_ficheiros(lista_de_veiculos, lista_de_clientes, lista_de_faturas)
 
-        elif op == "c":
-            lista_de_veiculos, lista_de_clientes, lista_de_faturas = carrega_as_listas_dos_ficheiros()
+        #elif op == "c":
+            #lista_de_veiculos, lista_de_clientes, lista_de_faturas = carrega_as_listas_dos_ficheiros()
 
-        elif op == "nc":
-            novo_cliente = cria_novo_cliente()
-            if novo_cliente is not None:
-                lista_de_clientes.append(novo_cliente)
+        #elif op == "nc":
+            #novo_cliente = cria_novo_cliente()
+            #if novo_cliente is not None:
+                #lista_de_clientes.append(novo_cliente)
 
-        elif op == "nv":
-            novo_veiculo = cria_novo_veiculo()
-            if novo_veiculo is not None:
-                lista_de_veiculos.append(novo_veiculo)
+        #elif op == "nv":
+            #novo_veiculo = cria_novo_veiculo()
+            #if novo_veiculo is not None:
+                #lista_de_veiculos.append(novo_veiculo)
 
-        elif op == "nf":
-            if len(lista_de_clientes) == 0 or len(lista_de_veiculos) == 0:
-                print("Não há clientes ou veiculos registados...")
-                continue
+        #elif op == "nf":
+            #if len(lista_de_clientes) == 0 or len(lista_de_veiculos) == 0:
+                #print("Não há clientes ou veiculos registados...")
+                #continue
 
-            nova_fatura = cria_nova_fatura(lista_de_clientes, lista_de_veiculos)
-            lista_de_faturas.append(nova_fatura)
+            #nova_fatura = cria_nova_fatura(lista_de_clientes, lista_de_veiculos)
+            #lista_de_faturas.append(nova_fatura)
 
-        elif op == "lc":
-            imprime_lista_de_clientes(lista_de_clientes)
-            pause()
+        #elif op == "lc":
+            #imprime_lista_de_clientes(lista_de_clientes)
+            #pause()
 
-        elif op == "lv":
-            imprime_lista_de_veiculos(lista_de_veiculos)
-            pause()
+        #elif op == "lv":
+            #imprime_lista_de_veiculos(lista_de_veiculos)
+            #pause()
 
-        elif op == "lf":
-            imprime_lista_de_faturas(lista_de_faturas)
-            pause()
+        #elif op == "lf":
+            #imprime_lista_de_faturas(lista_de_faturas)
+            #pause()
 
 
 if __name__ == "__main__":
