511eae2e5fbcc5569787b561155de6af3b9c3215
Ricardo Vilhena <a79305@ualg.pt>
2023-11-20 14:55:36 +0000

    Eliminei ficheiros clientes e copiei o código para o ficheiro cliente.py

--- base/src/clientes.py
+++ base/src/clientes.py
@@ -9,3 +9,21 @@ nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
 # todos os outros ficheiros clientes-*.py, e inclusive estes comentários
 
 # ...
+def cria_novo_cliente():
+    # Inicialize um dicionário vazio para armazenar as informações do cliente
+    novo_cliente = {}
+
+    # Solicite as informações do cliente ao usuário
+    novo_cliente["nome"] = input("Nome do cliente: ")
+    novo_cliente["nif"] = input("NIF do cliente: ")
+    novo_cliente["email"] = input("E-mail do cliente: ")
+    novo_cliente["telefone"] = input("Telefone do cliente: ")
+
+    # Returnar o dicionário com as informações do novo cliente
+    return novo_cliente
+
+def imprime_lista_de_clientes(lista_de_clientes):
+    """TODO: documentação"""
+
+    #TODO: Implementar esta função
+    # ...
